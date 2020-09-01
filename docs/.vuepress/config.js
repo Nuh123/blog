2b0a452c2754ef52055fec79c94168db09f6142e
Mayong <1345959121@qq.com>
2020-09-01 21:00:46 +0800

    性能优化类开坑

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -85,6 +85,16 @@ module.exports = {
 
                     ]
                 },
+                {
+                    title: '性能优化相关',   // 必要的
+                    //path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                    //collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 2,    // 可选的, 默认值是 1
+                    children: [
+                        ['./performance/base', '性能优化概述'],
+                        ['./performance/picture', '图片优化'],
+                    ]
+                },
             ],
 
             '/view/project/': [
